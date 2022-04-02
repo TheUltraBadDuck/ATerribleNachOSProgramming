6d4f4eaeeddeffed3ec364768804ade789285d17
TheUltraBadDuck <92137386+TheUltraBadDuck@users.noreply.github.com>
2022-04-02 20:23:06 +0700

    Add files via upload

--- NachOS-4.0/code/test/sort.c
+++ NachOS-4.0/code/test/sort.c
@@ -1,11 +1,11 @@
-/* sort.c 
- *    Test program to sort a large number of integers.
- *
- *    Intention is to stress virtual memory system.
- *
- *    Ideally, we could read the unsorted array off of the file system,
- *	and store the result back to the file system!
- */
+// /* sort.c 
+//  *    Test program to sort a large number of integers.
+//  *
+//  *    Intention is to stress virtual memory system.
+//  *
+//  *    Ideally, we could read the unsorted array off of the file system,
+//  *	and store the result back to the file system!
+//  */
 
 
 /*
@@ -20,12 +20,11 @@
 #include "syscall.h"
 #endif /* UNIX */
 
-#define SIZE (1024)
+#define SIZE (20)
 
 int A[SIZE];	/* size of physical memory; with code, we'll run out of space!*/
 
-int
-main()
+int main()
 {
     int i, j, tmp;
 
@@ -64,7 +63,10 @@ main()
         if (((i + 1) % 15) == 0)
             PrintChar('\n');
         if (A[i] != i) {
-            PrintString("Out of order A[");
+            PrintChar('!');
+            PrintChar('!');
+            PrintChar('!');
+//            PrintString("Out of order A[");
             PrintNum(i);
             PrintString("] = ");
             PrintNum(A[i]);
@@ -82,3 +84,79 @@ main()
 
     Exit(0);
 }
+
+// #include "syscall.h"
+// #define MAX_CHAR_SIZE 20
+
+
+// void printArray(int arr[], int i) {
+//     PrintChar('\n');
+//     for (i = 0; i < MAX_CHAR_SIZE; i++) {
+//         PrintNum(arr[i]);
+//         PrintChar('\t');
+//         if ((i + 1) % 8 == 0)
+//             PrintChar('\n');
+//     }
+//     PrintChar('\n');
+// }
+
+
+// void bubbleSort(int arr[], char order) {       // true: from smallest to largest, false: from largest to smallest
+//     int i, j;
+//     int c;
+//     char swapping;
+//     for (i = 0; i < MAX_CHAR_SIZE - 1; i++) {
+//         swapping = 0;
+//         for (j = 0; j < MAX_CHAR_SIZE - i - 1; j++) {
+//             if (order) {
+//                 if (arr[j] > arr[j + 1]) {
+//                     c = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = c;
+//                     swapping = 1;
+//                 }
+//             }
+//             else {
+//                 if (arr[j] < arr[j + 1]) {
+//                     c = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = c;
+//                     swapping = 1;
+//                 }
+//             }
+//         }
+
+//         if (swapping == 0) {
+//             break;
+//         }
+//     }
+// }
+
+
+// int main() {
+//     int arr[MAX_CHAR_SIZE];
+//     int i;
+//     char c;
+//     char order;
+
+//     for (i = 0; i < MAX_CHAR_SIZE; i++) {
+//         arr[i] = RandomNum() % 255;
+//     }
+
+//     //PrintString("Before: ");
+//     printArray(arr, i);
+
+//     //PrintString("Sorting from smallest to largest (Y) or sorting from smallest to largest (N)? Default Y. ");
+//     c = ReadChar();
+//     if ((c == 'N') || (c == 'n'))
+//         order = 0;
+//     else
+//         order = 1;
+
+//     bubbleSort(arr, order);
+
+//     //PrintString("After: ");
+//     printArray(arr, i);
+
+//     Halt();
+// }
